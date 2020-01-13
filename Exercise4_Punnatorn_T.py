1288f2014bcf7bdd3c0498abeeed741861839f82
30th-th <30th-th@users.noreply.github.com>
2020-01-13 23:47:14 +0700

    Create Exercise4_Punnatorn_T.py
    Master

---
+++ Exercise4_Punnatorn_T.py
@@ -0,0 +1,11 @@
+FE = 80.0
+GB = 80.0
+CSYS = 80.0
+CPRO = 80.0
+
+
+print('--- Your Score ---')
+print('Foundation English : ',FE)
+print('General Business : ',GB)
+print('Introduction to Computer Systems : ',CSYS)
+print('Computer Programming : ',CPRO)
